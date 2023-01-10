d890ebd0598c65c70231dfa0c506d18d373e218d
Sebastian Bille <sebastianbille@outlook.com>
2023-01-10 09:53:40 +0100

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Serverless Manifesto</title>
         <meta name='description' content='A Serverless Manifesto' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div>
